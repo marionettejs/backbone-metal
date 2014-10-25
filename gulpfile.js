df9c42195ef5cfd4a6267bc2727bacff99f78da8
James Kyle <me@thejameskyle.com>
2014-10-25 09:16:25 -0700

    Init documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp');
+var gulp       = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var preprocess = require('gulp-preprocess');
-var to5 = require('gulp-6to5');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
-var mocha = require('gulp-mocha');
-var package = require('./package');
+var to5        = require('gulp-6to5');
+var rename     = require('gulp-rename');
+var uglify     = require('gulp-uglify');
+var filter     = require('gulp-filter');
+var jshint     = require('gulp-jshint');
+var stylish    = require('jshint-stylish');
+var mocha      = require('gulp-mocha');
+var dox        = require('gulp-dox');
+var package    = require('./package');
 
 gulp.task('build', function() {
   return gulp.src('src/wrapper.js')
@@ -24,6 +25,12 @@ gulp.task('build', function() {
       .pipe(rename({ extname: '.min.js' }))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('docs', function() {
+  return gulp.src('src/metal.js')
+    .pipe(dox())
+    .pipe(gulp.dest('docs/'));
 });
 
 gulp.task('jshint', function() {
