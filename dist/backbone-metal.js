10dbbf6d894be39f55271cf0590a7f1a1edeed79
James Kyle <me@thejameskyle.com>
2015-06-30 04:19:16 +0100

    Build lib

--- dist/backbone-metal.js
+++ dist/backbone-metal.js
@@ -1,21 +1,42 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('underscore'), require('backbone')) :
-  typeof define === 'function' && define.amd ? define(['underscore', 'backbone'], factory) :
-  global.Metal = factory(global._, global.Backbone)
-}(this, function (_, Backbone) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('backbone'), require('underscore')) : typeof define === 'function' && define.amd ? define(['backbone', 'underscore'], factory) : global.Metal = factory(global.Backbone, global._);
+})(this, function (Backbone, _) {
+  'use strict';
 
-  var Metal = Backbone.Metal = {};
+  var mixin__Mixin = function Mixin(protoProps) {
+    // Add prototype properties (instance properties) to the class, if supplied.
+    _.extend(this, protoProps);
+  };
 
   /**
-   * Wraps the passed method so that `this._super` will point to the superMethod
-   * when the method is invoked.
+   * Checks if `value` is a Metal Mixin.
    *
-   * @private
-   * @method wrap
-   * @param {Function} method - The method to call.
-   * @param {Function} superMeqthod - The super method.
-   * @return {Function} - wrapped function.
+   * ```js
+   * Mixin.isMixin(new Mixin());
+   * // >> true
+   * Mixin.isMixin({});
+   * // >> false
+   * Mixin.isMixin(function() {...});
+   * // >> false
+   * Mixin.isMixin(new Class());
+   * // >> false
+   * ```
+   *
+   * @public
+   * @method isMixin
+   * @memberOf _
+   * @param {*} value - The value to check.
    */
+  mixin__Mixin.isMixin = function isMixin(value) {
+    return !!value && value instanceof mixin__Mixin;
+  };
+
+  var mixin = mixin__Mixin;
+
+  var Events = new mixin(Backbone.Events);
+
+  var events = Events;
+
   function _wrap(method, superMethod) {
     return function () {
       var prevSuper = this._super;
@@ -32,7 +53,9 @@
    * @private
    * @const {RegExp}
    */
-  var CONTAINS_SUPER = /xyz/.test(new Function("xyz")) ? /\b_super\b/ : /.*/;
+  var CONTAINS_SUPER = /xyz/.test(function () {
+    xyz;
+  }) ? /\b_super\b/ : /.*/; // eslint-disable-line
 
   /**
    * Assigns properties of source object to destination object, wrapping methods
@@ -46,11 +69,11 @@
   function _wrapAll(dest, source) {
     var keys = _.keys(source),
         length = keys.length,
-        i,
-        name,
-        method,
-        superMethod,
-        hasSuper;
+        i = undefined,
+        name = undefined,
+        method = undefined,
+        superMethod = undefined,
+        hasSuper = undefined;
 
     for (i = 0; i < length; i++) {
       name = keys[i];
@@ -76,7 +99,7 @@
    * Creates a new Class.
    *
    * ```js
-   * var MyClass = Class.extend({
+   * const MyClass = Class.extend({
    *   initialize() {
    *     console.log('Created!');
    *   }
@@ -91,32 +114,53 @@
    * @memberOf Metal
    * @memberOf Backbone
    */
-  var Class = Metal.Class = function () {
-    var _ref;
-    (_ref = this).initialize.apply(_ref, arguments);
+  var _class__Class = function Class() {
+    this.cid = _.uniqueId(this.cidPrefix);
+    this.initialize.apply(this, arguments);
   };
 
-  /**
-   * An overridable method called when objects are instantiated. Does not do
-   * anything by default.
-   *
-   * @public
-   * @abstract
-   * @method initialize
-   */
-  Class.prototype.initialize = _.noop;
+  _.extend(_class__Class.prototype, {
 
-  _.extend(Class, {
+    /**
+     * Identifier prefix of the cid. It can be override.
+     *
+     * @public
+     * @attributeOf Class {String}
+     */
+    cidPrefix: 'metal',
+
+    /**
+     * An overridable method called when objects are instantiated. Does not do
+     * anything by default.
+     *
+     * @public
+     * @abstract
+     * @method initialize
+     */
+    initialize: function initialize() {},
+
+    /**
+     * Destroy a Class by removing all listeners.
+     *
+     * @public
+     * @method destroy
+     */
+    destroy: function destroy() {
+      this.stopListening();
+    }
+  });
+
+  _.extend(_class__Class, {
 
     /**
      * Creates a new subclass.
      *
      * ```js
-     * var MyClass = Class.extend({
+     * const MyClass = Class.extend({
      *   // ...
      * });
      *
-     * var myClass = new MyClass();
+     * const myClass = new MyClass();
      * myClass instanceof MyClass
      * // true
      * myClass instanceof Class
@@ -131,12 +175,12 @@
      */
     extend: function extend(protoProps, staticProps) {
       var Parent = this;
-      var Child;
+      var Child = undefined;
 
       // The constructor function for the new subclass is either defined by you
       // (the "constructor" property in your `extend` definition), or defaulted
       // by us to simply call the parent's constructor.
-      if (!protoProps || !_.has(protoProps, "constructor")) {
+      if (!protoProps || !_.has(protoProps, 'constructor')) {
         Child = function () {
           Parent.apply(this, arguments);
         };
@@ -154,9 +198,10 @@
 
       // Set the prototype chain to inherit from `parent`, without calling
       // `parent`'s constructor function.
-      var Surrogate = function () {
+      var Surrogate = function Surrogate() {
         this.constructor = Child;
       };
+
       Surrogate.prototype = Parent.prototype;
       Child.prototype = new Surrogate();
 
@@ -180,13 +225,13 @@
      * Mixes properties onto the class's prototype.
      *
      * ```js
-     * var MyMixin = new Mixin({
+     * const MyMixin = new Mixin({
      *   alert() {
      *     console.log('Alert!');
      *   }
      * });
      *
-     * var MyClass = Class.extend({
+     * const MyClass = Class.extend({
      *   initialize() {
      *     this.alert();
      *   }
@@ -214,13 +259,13 @@
      * Mixes properties onto the class's constructor.
      *
      * ```js
-     * var MyMixin = new Mixin({
+     * const MyMixin = new Mixin({
      *   alert() {
      *     console.log('Alert!');
      *   }
      * });
      *
-     * var MyClass = Class.extend(...);
+     * const MyClass = Class.extend(...);
      *
      * MyClass.include(MyMixin);
      *
@@ -231,7 +276,7 @@
      * You can also simply pass a plain javascript object.
      *
      * ```js
-     * var MyClass = Class.extend(...);
+     * const MyClass = Class.extend(...);
      *
      * MyClass.include({
      *   alert() {
@@ -259,13 +304,13 @@
      * Checks if `value` is a Metal Class.
      *
      * ```js
-     * _.isClass(Class.extend(...));
+     * Class.isClass(Class.extend(...));
      * // >> true
-     * _.isClass(new Class());
+     * Class.isClass(new Class());
      * // >> true
-     * _.isClass(function() {...});
+     * Class.isClass(function() {...});
      * // >> false
-     * _.isClass({...});
+     * Class.isClass({...});
      * // >> false
      * ```
      * @public
@@ -274,52 +319,19 @@
      * @param {*} value - The value to check.
      */
     isClass: function isClass(value) {
-      return !!value && (value instanceof Class || value.prototype instanceof Class || value === Class);
+      return !!value && (value instanceof _class__Class || value.prototype instanceof _class__Class || value === _class__Class);
     }
   });
 
   /**
-   * Allows you to create mixins, whose properties can be added to other classes.
-   *
-   * @public
-   * @class Mixin
-   * @memberOf Metal
-   * @memberOf Backbone
-   * @param {Object} protoProps - The properties to be added to the prototype.
+   * @class Class
+   * @mixes Events
    */
-  var Mixin = Metal.Mixin = function (protoProps) {
-    // Add prototype properties (instance properties) to the class, if supplied.
-    _.extend(this, protoProps);
-  };
+  _class__Class.mixin(events);
 
-  /**
-   * Checks if `value` is a Metal Mixin.
-   *
-   * ```js
-   * _.isMixin(new Mixin());
-   * // >> true
-   * _.isMixin({});
-   * // >> false
-   * _.isMixin(function() {...});
-   * // >> false
-   * _.isMixin(new Class());
-   * // >> false
-   * ```
-   *
-   * @public
-   * @method isMixin
-   * @memberOf _
-   * @param {*} value - The value to check.
-   */
-  Mixin.isMixin = function (value) {
-    return !!value && value instanceof Mixin;
-  };
+  var _class = _class__Class;
 
-  /**
-   * @private
-   * @const {String[]}
-   */
-  var ERROR_PROPS = ["description", "fileName", "lineNumber", "name", "message", "number"];
+  var ERROR_PROPS = ['description', 'fileName', 'lineNumber', 'name', 'message', 'number'];
 
   /**
    * A subclass of the JavaScript Error.
@@ -335,7 +347,7 @@
    * @extends Error
    * @uses Metal.Class
    */
-  var Err = Metal.Error = Class.extend.call(Error, {
+  var Err = _class.extend.call(Error, {
 
     /**
      * @public
@@ -346,6 +358,7 @@
      */
     constructor: function constructor(message) {
       var options = arguments[1] === undefined ? {} : arguments[1];
+
       // If options are provided in place of a message, assume message exists on
       // options.
       if (_.isObject(message)) {
@@ -377,7 +390,7 @@
      * @returns {String} - Formatted error message.
      */
     toString: function toString() {
-      return "" + this.name + ": " + this.message;
+      return '' + this.name + ': ' + this.message;
     }
   }, {
 
@@ -398,19 +411,12 @@
    * @class Error
    * @mixes Class
    */
-  _.extend(Err, Class);
+  _.extend(Err, _class);
 
-  /**
-   * Display a deprecation warning with the provided message.
-   *
-   * @public
-   * @method deprecate
-   * @param {String|Object} message - A description of the deprecation.
-   * @param {String} message.prev - The deprecated item.
-   * @param {String} message.next - The replacement for the deprecated item.
-   * @param {Boolean} [test] - An optional boolean. If falsy, the deprecation will be displayed.
-   */
-  var deprecate = Metal.deprecate = function (message, test) {
+  var _error = Err;
+
+  var _deprecate__deprecate = function deprecate(message, test) {
+
     // Returns if test is provided and is falsy.
     if (test !== undefined && test) {
       return;
@@ -418,16 +424,16 @@
 
     // If message is provided as an object, format the object into a string.
     if (_.isObject(message)) {
-      message = deprecate._format(message.prev, message.next);
+      message = _deprecate__deprecate._format(message.prev, message.next);
     }
 
     // Ensure that message is a string
     message = message && message.toString();
 
     // If deprecation message has not already been warned, send the warning.
-    if (!deprecate._cache[message]) {
-      deprecate._warn("Deprecation warning: " + message);
-      deprecate._cache[message] = true;
+    if (!_deprecate__deprecate._cache[message]) {
+      _deprecate__deprecate._warn('Deprecation warning: ' + message);
+      _deprecate__deprecate._cache[message] = true;
     }
   };
 
@@ -441,20 +447,38 @@
    * @param {String} next - The replacement for the deprecated item.
    * @return {String} - The formatted message.
    */
-  deprecate._format = function (prev, next) {
-    return "" + prev + " is going to be removed in the future. Please use " + next + " instead.";
+  _deprecate__deprecate._format = function _format(prev, next) {
+    return '' + prev + ' is going to be removed in the future. Please use ' + next + ' instead.';
   };
 
   /**
-   * A safe reference to `console.warn` that will fallback to `console.log` or
-   * `_noop` if the `console` object does not exist.
+   * A safe reference to the console object that will fallback to an empty
+   * object.
+   *
+   * @private
+   * @property _console
+   */
+  var _console = typeof console !== 'undefined' ? console : {};
+
+  /**
+   * A safe reference to the console.warn method that will fallback a noop.
+   *
+   * @private
+   * @property _warnMethod
+   */
+  var _warnMethod = _console.warn || _console.log || function () {};
+
+  /**
+   * A safe reference to `console.warn`.
    *
    * @private
    * @method _warn
    * @memberOf deprecate
    * @param {*...} - The values to warn in the console.
    */
-  deprecate._warn = typeof console !== "undefined" && (console.warn || console.log) || _.noop;
+  _deprecate__deprecate._warn = function _warn() {
+    return _warnMethod.apply(_console, arguments);
+  };
 
   /**
    * An internal cache to avoid sending the same deprecation warning multiple
@@ -464,27 +488,18 @@
    * @property _cache
    * @memberOf deprecate
    */
-  deprecate._cache = {};
+  _deprecate__deprecate._cache = {};
 
-  /**
-   * A `Metal.Mixin` version of `Backbone.Events`.
-   *
-   * @mixin Events
-   * @memberOf Metal
-   * @memberOf Backbone
-   * @extends Metal.Mixin
-   * @mixes Backbone.Events
-   */
-  var Events = Metal.Events = new Mixin(Backbone.Events);
+  var _deprecate = _deprecate__deprecate;
 
-  /**
-   * @class Class
-   * @mixes Events
-   */
-  Class.mixin(Events);
+  var backbone_metal = Backbone.Metal = {
+    Class: Class,
+    Mixin: Mixin,
+    Error: _error,
+    deprecate: deprecate,
+    Events: Events
+  };
 
-
-  return Metal;
-
-}));
+  return backbone_metal;
+});
 //# sourceMappingURL=./backbone-metal.js.map
